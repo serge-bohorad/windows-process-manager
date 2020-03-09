484d9807c9630fd3a04dee2b7b5d740793f2251d
iRandell <irandellhart@gmail.com>
2020-03-09 11:02:54 +0200

    update function closeHandle | remove comment

--- src/typescript/close-handle/index.ts
+++ src/typescript/close-handle/index.ts
@@ -1,5 +1,6 @@
 const pm = require('bindings')('windows-process-manager')
 
+// TODO: create the function 'closeSeveralHandles'
 export function closeHandle(handle: Buffer): void {
   if (!Buffer.isBuffer(handle)) {
     throw TypeError('Unable to close handle! The first argument is not a Buffer.')
